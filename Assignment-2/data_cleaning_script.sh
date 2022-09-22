d0f5fc590a389d478a4386383b28206b0f729590
adityaemmanuel <40785524+adityaemmanuel@users.noreply.github.com>
2022-09-22 15:01:11 +0000

    removed extra else condition

--- Assignment-2/data_cleaning_script.sh
+++ Assignment-2/data_cleaning_script.sh
@@ -4,15 +4,16 @@
 read -p "Enter the name of the file you want to clean: " full_file_name
 
 # Check if the file exists
-if [ -f "$full_file_name" ]; then
-    echo "File exists"
-else
+if [ ! -f "$full_file_name" ]; then
     echo "File does not exist"
     exit 1
 fi
 
 # Select top n rows
 read -p "Enter the number of rows you want to select: " n
-IFS='.' read -ra file_name <<< "$full_file_name"
-echo $file_name
-head -n $n $full_file_name > "${file_name}_processed.csv"
+# check in n is greater than 0
+if [ $n -gt 0 ]; then
+    IFS='.' read -a file_name <<< "$full_file_name"
+    head -n $n $full_file_name > "${file_name[0]}_processed.csv"
+    echo "File created: ${file_name[0]}_processed.csv"
+fi
